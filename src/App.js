04de415f6b07d086625fca57c020d600a614e360
Dima Snisarenko <snisarenkodima@gmail.com>
2017-10-01 08:58:26 +0200

    feat(css): Add styleing with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            <nav className="navbar navbar-inverse navbar-fixed-top">
+                <div className="container">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="/">Readable</a>
+                    </div>
+                </div>
+            </nav>
+
+            <div className="container">
+            </div>
+        </div>
     );
   }
 }
