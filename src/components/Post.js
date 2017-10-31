de423ff7f31db758f9ecbbee08d6dd667e97611e
Dima Snisarenko <snisarenkodima@gmail.com>
2017-10-31 19:16:14 +0100

    feat(post): vote/edit/delete in the post list

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { api } from "../utils/api";
 import { withRouter } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import { deletePost } from "../actions/index";
 import PostSummary from './PostSummary';
 import PostComments from './PostComments';
 import { findPost } from "../utils/find-post";
 
 class Post extends Component {
-  delete(event) {
-    event.preventDefault();
-    if (!window.confirm('Are you sure')) {
-      return;
-    }
-
-    api.deletePost(this.props.post.id).then((post) => {
-      this.props.dispatch(deletePost(post));
-      this.props.history.replace(`/category/${post.category}`);
-    })
+  onDelete(post) {
+    this.props.history.replace(`/category/${post.category}`);
   }
 
   render() {
@@ -30,19 +19,8 @@ class Post extends Component {
     return (
       <div>
         <h1>{post.title}</h1>
-        <p>
-          <Link to={`/edit/post/${post.id}`}>
-            <i className="glyphicon glyphicon-edit" />
-            Edit
-          </Link>
-          &nbsp;
-          <button className="btn-link" onClick={e => this.delete(e)}>
-            <i className="glyphicon glyphicon-remove" />
-            Delete
-          </button>
-        </p>
         <p>{post.body}</p>
-        <PostSummary id={post.id} allowVoting={true} />
+        <PostSummary id={post.id} onDelete={(post) => this.onDelete(post)} />
         <hr />
         <PostComments id={post.id} />
       </div>
--- src/components/PostSummary.js
+++ src/components/PostSummary.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 import { api } from "../utils/api";
-import { updatePost } from "../actions/index";
+import { updatePost, deletePost } from "../actions/index";
 import { findPost } from "../utils/find-post";
+import { Link } from 'react-router-dom'
 
 class PostSummary extends Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
-    allowVoting: PropTypes.bool,
-    onVote: PropTypes.func,
+    onDelete: PropTypes.func,
   }
 
   vote(event, option) {
@@ -19,29 +19,48 @@ class PostSummary extends Component {
     });
   }
 
+  delete(event) {
+    event.preventDefault();
+    if (!window.confirm('Are you sure')) {
+      return;
+    }
+
+    api.deletePost(this.props.post.id).then((post) => {
+      this.props.dispatch(deletePost(post));
+      this.props.onDelete && this.props.onDelete(post);
+    })
+  }
+
   render() {
     const post = this.props.post;
     return (
-      <div>
-        <span className="badge">{post.voteScore}</span>
+      <div className="row">
+        <div className="col-xs-7">
+          <span className="badge">{post.voteScore}</span>
 
-        {
-          this.props.allowVoting
-          ? (
-            <span>
-              <button className="btn-link" onClick={event => this.vote(event, 'upVote')}><i className="glyphicon glyphicon-thumbs-up" /></button>
-              <button className="btn-link" onClick={event => this.vote(event, 'downVote')}><i className="glyphicon glyphicon-thumbs-down" /></button>
-            </span>
-            )
-          : ''
-        }
+          <button className="btn-link" onClick={event => this.vote(event, 'upVote')}><i className="glyphicon glyphicon-thumbs-up" /></button>
+          <button className="btn-link" onClick={event => this.vote(event, 'downVote')}><i className="glyphicon glyphicon-thumbs-down" /></button>
 
-        &nbsp;
-        Created
-        &nbsp;
-        <em>{new Date(post.timestamp).toLocaleString()}</em>
-        &nbsp;
-        by <strong>{post.author}</strong>
+          &nbsp;
+          Created
+          &nbsp;
+          <em>{new Date(post.timestamp).toLocaleString()}</em>
+          &nbsp;
+          by <strong>{post.author}</strong>
+        </div>
+        <div className="col-xs-5 text-right">
+          <p>
+            <Link to={`/edit/post/${post.id}`}>
+              <i className="glyphicon glyphicon-edit" />
+              Edit
+            </Link>
+            &nbsp;
+            <button className="btn-link" onClick={e => this.delete(e)}>
+              <i className="glyphicon glyphicon-remove" />
+              Delete
+            </button>
+          </p>
+        </div>
       </div>
     )
   }
